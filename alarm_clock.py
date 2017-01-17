782061f7e8e1a07eec08f550dc35b876278b7667
joehtet <joehtet@gmail.com>
2017-01-17 13:22:46 -0500

    refactoring and cleaning

--- alarm_clock.py
+++ alarm_clock.py
@@ -3,48 +3,57 @@ import time
 import random
 import argparse
 
-# returns total time in seconds
+
+# opens a random link in web browser
+def open_link(links):
+    webbrowser.open(random.choice(links), new=0, autoraise=True)
+       
+# converts total time to seconds
 def get_seconds(hours, minutes, seconds):
     time_in_seconds = hours*60*60 + minutes*60 + seconds
     print("Alarm set for {} hours, {} minutes, {} seconds".format(hours,minutes,seconds))
     return time_in_seconds
 
-# sets the alarm
-def set_alarm(time_in_seconds):
+# sets timer
+def set_alarm(time_in_seconds, links):
     time.sleep(time_in_seconds)
-    
-    # open a random link from links in web browser
-    webbrowser.open(random.choice(links), new=0, autoraise=True)
+    open_link(links)
 
 # start pomodoro timer
-def pomodoro():
+def pomodoro(links):
     print("Started pomodoro timer, alarm will ring in 25 minutes.")
     
     while True:
         time.sleep(25*60)
         print("Time to take a break.")
-        webbrowser.open(random.choice(links), new=0, autoraise=True)
+        open_link(links)
+
         time.sleep(5*60)
         print("Time to get back to work.")
-        webbrowser.open(random.choice(links), new=0, autoraise=True)
+        open_link(links)
+
 
 # gets user inputted time from cmd line
 parser = argparse.ArgumentParser(description='Set the alarm clock or start pomodoro timer.')
 
-parser.add_argument('-p', '--pomodoro', action='store_true', default=False,
-                    dest='pomodoro')
+group = parser.add_mutually_exclusive_group()
+
+group.add_argument('-p', '--pomodoro', action='store_true', default=False,
+                    dest='pomodoro', help='start the pomodoro timer (25 minutes of work, 5 minutes break time)')
+group.add_argument('-t', '--timer' , action='store_true', default=False, dest='timer', help='start the timer, use -hr HOUR -m MINUTES -s SECONDS')
 parser.add_argument('-hr', '--hours', nargs='?', default=0, type=int)
 parser.add_argument('-m', '--minutes', nargs='?', default=0, type=int)
 parser.add_argument('-s', '--seconds', nargs='?', default=0, type=int)
 args = parser.parse_args()
+
 
 # gets list of youtube links from file
 with open("alarm_songs.txt", "r") as f:
     links = f.readlines()
 
 if args.pomodoro:
-    pomodoro()
+    pomodoro(links)
+elif args.timer:
+    set_alarm(get_seconds( args.hours, args.minutes, args.seconds), links)
 else:
-    set_alarm(get_seconds( args.hours, args.minutes, args.seconds))
-
-
+    print("Do nothing")
