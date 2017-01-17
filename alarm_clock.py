ac322d15ae7cd9747d9680be13b380e8c6cb09ba
joehtet <joehtet@gmail.com>
2017-01-17 11:34:50 -0500

    updated

--- alarm_clock.py
+++ alarm_clock.py
@@ -3,31 +3,32 @@ import time
 import random
 import argparse
 
-
-def get_time(hours, minutes, seconds):
+# returns total time in seconds
+def get_seconds(hours, minutes, seconds):
     time_in_seconds = hours*60*60 + minutes*60 + seconds
     print("Alarm set for {} hours, {} minutes, {} seconds".format(hours,minutes,seconds))
     return time_in_seconds
 
 # sets the alarm
 def set_alarm(time_in_seconds):
-    time.sleep(time_in_seconds)
-    
-    # creates a list of youtube links from file
+    # gets list of youtube links from file
     with open("alarm_songs.txt", "r") as f:
         links = f.readlines()
+
+    time.sleep(time_in_seconds)
     
     # open a random link from links in web browser
     webbrowser.open(random.choice(links), new=0, autoraise=True)
 
 
-# gets input for alarm from cmd line
+# gets user inputted time from cmd line
 parser = argparse.ArgumentParser(description='Set the alarm clock.')
-parser.add_argument('-hr', '--hours', nargs='?', const=0, default=0, type=int)
-parser.add_argument('-m', '--minutes', nargs='?', const=0, default=0, type=int)
-parser.add_argument('-s', '--seconds', nargs='?', const=0, default=0, type=int)
+parser.add_argument('-hr', '--hours', nargs='?', default=0, type=int)
+parser.add_argument('-m', '--minutes', nargs='?', default=0, type=int)
+parser.add_argument('-s', '--seconds', nargs='?', default=0, type=int)
 args = parser.parse_args()
 
-set_alarm(get_time( args.hours, args.minutes, args.seconds))
+
+set_alarm(get_seconds( args.hours, args.minutes, args.seconds))
 
 
